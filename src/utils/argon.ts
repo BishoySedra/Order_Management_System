fb7e2fa15117048e641c16e1e8f4a39badad7664
Bishoy Sedra <bishosedra0@gmail.com>
2024-06-28 19:07:44 +0300

    Deleted branch and files!

--- src/utils/argon.ts
+++
@@ -1,9 +0,0 @@
-import * as argon from 'argon2';
-
-export const hashPassword = async (password: string): Promise<string> => {
-  return argon.hash(password);
-};
-
-export const verifyPassword = async (hash: string, password: string): Promise<boolean> => {
-  return argon.verify(hash, password);
-};
